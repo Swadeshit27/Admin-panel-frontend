dee68e7f009f2cd8d12177aa69b3a4a270483f1a
Mitali Raj <mitali.btmtcs22215@nfsu.ac.in>
2024-01-18 18:41:26 +0530

    Changes made to UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import MainContent from "./Components/MainContent";
 import Sidebar from "./Components/Sidebar";
 import TopBar from "./Components/TopBar";
+import './assets/App.css'
+import Home from './Components/Home'
+import Header from './Components/Header'
+
 
 const App = () => {
   return (
-    <div className="w-full h-screen flex flex-wrap">
-      <div className="w-[20%]">
-        <Sidebar />
-      </div>
-      <div className="w-[80%] relative">
-        <TopBar />
-        <MainContent />
-      </div>
+    <div className="grid-container">
+     <Header/>
+     <Sidebar/>
+     <Home />
     </div>
   );
 };
