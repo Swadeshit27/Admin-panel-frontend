c4a800e3cfb8e4900ef524a9c94b85db5b35eee1
Mitali Raj <mitali.btmtcs22215@nfsu.ac.in>
2024-01-31 15:01:10 +0530

    changes made

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import {BsFillBellFill,BsFillEnvelopeFill,BsPersonCircle,BsSearch,BsJustify} from 'react-icons/bs'
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs';
 
-function Header({OpenSidebar}) {
+function Header({ OpenSidebar }) {
   return (
     <header className='header'>
       <div className="menu-icon">
-        <BsJustify className='icon' onClick={OpenSidebar}/>
+        <BsJustify className='icon' onClick={OpenSidebar} />
       </div>
       <div className="header-left">
-        <BsSearch className='icon'/>
+        <BsSearch className='icon' />
+        <input type="text" placeholder="Search..." className="search-input" />
       </div>
       <div className="header-right">
-        <BsFillBellFill className='icon'/>
-        <BsFillEnvelopeFill className='icon'/>
-        <BsPersonCircle className='icon'/>
-
+        <BsFillBellFill className='icon' />
+        <BsFillEnvelopeFill className='icon' />
+        <BsPersonCircle className='icon' />
       </div>
     </header>
   );
